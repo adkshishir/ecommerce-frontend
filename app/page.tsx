878eb43fdfe78a96a5ddfe04f066e3ca007c8d06
adkshishir <98789290+adkshishir@users.noreply.github.com>
2024-04-05 23:31:41 +0545

    Seperate Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,97 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Carousel from "./components/Carousel/Carousel";
+import FeatureCard from "./components/cards/FeatureCard";
+import { CategoryCard } from "./components/cards/CategoryCard";
+import ProductCard from "./components/cards/ProductCard";
+import SpecialOfferCard from "./components/cards/SpecialOfferCard";
+import Vendor from "./components/Carousel/Vendor";
 
 export default function Home() {
+    let name='shsihir'
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   <main >
+    <button>{name}</button>
+         <Carousel/> 
+          {/* Featured Start */}
+          <div className="container-fluid pt-5">
+            <div className="row px-xl-5 pb-3">
+              <FeatureCard title="Free Shipping" icon="fa fa-shipping-fast"/>
+              <FeatureCard title="Return Policy" icon="fa fa-retweet"/>
+              <FeatureCard title="Quality Product" icon="fa fa-check"/>
+              <FeatureCard title="Secure Payment" icon="fa fa-credit-card"/>
+            </div>
+          </div>
+          {/* Featured End */}
+          {/* Categories Start */}
+          <div className="container-fluid pt-5">
+            <h2 className="section-title position-relative text-uppercase mx-xl-5 mb-4"><span className="bg-secondary pr-3">Categories</span></h2>
+            <div className="row px-xl-5 pb-3">
+              <CategoryCard image="/img/cat-1.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-2.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-3.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-4.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-1.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-2.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-3.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-4.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-1.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-3.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-2.jpg" categoryName="Category Name" href="/" productCount={100}/>
+              <CategoryCard image="/img/cat-4.jpg" categoryName="Category Name" href="/" productCount={100}/>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            </div>
+          </div>
+          {/* Categories End */}
+          {/* Products Start */}
+          <div className="container-fluid pt-5 pb-3">
+            <h2 className="section-title position-relative text-uppercase mx-xl-5 mb-4"><span className="bg-secondary pr-3">Featured Products</span></h2>
+            <div className="row px-xl-5">
+            <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-2.jpg" name="Product Name" price={124} stock={90}/>
+            <ProductCard image="/img/product-3.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-4.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-3.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-4.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-3.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-4.jpg" name="Product Name" price={123} stock={100}/>
+            <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>     
+            </div>
+          </div>
+          {/* Products End */}
+          {/* Offer Start */}
+          <div className="container-fluid pt-5 pb-3">
+            <div className="row px-xl-5">
+              <SpecialOfferCard image="/img/offer-1.jpg" discount="20%" href='/' className="col-md-6"/>
+              <SpecialOfferCard image="/img/offer-2.jpg" discount="20%" href='/' className="col-md-6"/>         
+            </div>
+          </div>
+          {/* Offer End */}
+          {/* Products Start */}
+          <div className="container-fluid pt-5 pb-3">
+            <h2 className="section-title position-relative text-uppercase mx-xl-5 mb-4"><span className="bg-secondary pr-3">Recent Products</span></h2>
+            <div className="row px-xl-5">
+              <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>
+              <ProductCard image="/img/product-2.jpg" name="Product Name" price={124} stock={90}/>
+              <ProductCard image="/img/product-3.jpg" name="Product Name" price={123} stock={100}/>
+              <ProductCard image="/img/product-4.jpg" name="Product Name" price={123} stock={100}/>
+              <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>
+              <ProductCard image="/img/product-3.jpg" name="Product Name" price={123} stock={100}/>
+              <ProductCard image="/img/product-4.jpg" name="Product Name" price={123} stock={100}/>
+              <ProductCard image="/img/product-1.jpg" name="Product Name" price={123} stock={100}/>
+            </div>
+          </div>
+          {/* Products End */}
+          {/* Vendor Start */}
+          <div className="container-fluid py-5">
+            <div className="row px-xl-5">
+              <Vendor/>
+            </div>
+          </div>
+          {/* Vendor End */}
+      
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+   </main>
   );
 }
