a1434d2132d3472e21d8c6f80170e8ef1ed6b05e
adkshishir <adhikarishishir50@gmail.com>
2024-05-23 10:36:43 +0545

    fix : not showing nav bar

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
   return (
     <MyProvider>
       <SetData homeData={homeData} products={products} />
-      {homeData.success && <Header />}
+      {<Header />}
       {children}
       <Footer />
     </MyProvider>
