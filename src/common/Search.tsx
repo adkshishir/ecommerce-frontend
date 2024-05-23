036d620761ec33487a581a15a2adf6538bb255c3
adkshishir <adhikarishishir50@gmail.com>
2024-05-23 10:39:25 +0545

    refactor : search bar

--- src/common/Search.tsx
+++ src/common/Search.tsx
@@ -1,12 +1,9 @@
 'use client';
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import CircularProgress from '@mui/material/CircularProgress';
-
 import Api from '../constants/Api';
 import Request from '../helper/Request';
-import { MyContext } from '../context/MyProvider';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 interface Product {
   name: string;
@@ -14,6 +11,7 @@ interface Product {
 }
 
 export default function Search() {
+  const router = useRouter();
   const [open, setOpen] = React.useState(false);
   const [products, setProducts] = React.useState<any>([]);
   const [search, setSearch] = React.useState<string>('');
@@ -24,12 +22,17 @@ export default function Search() {
     if (!open) {
       setOptions([]);
     }
-    async function getProducts() {
+    getProducts();
+  }, [open, search]);
+  async function getProducts() {
+    try {
       loading = true;
       setOptions([]);
       setTimeout(async () => {
-        let fetchProducts = await Request.get(Api.SEARCH_PRODUCTS + search);
-        if (fetchProducts.data.length > 0) setOptions(await fetchProducts.data);
+        let fetchProducts = await Request.get(Api.SEARCH_PRODUCTS);
+        setProducts(await fetchProducts.data.products);
+        if (fetchProducts.data.products.length > 0)
+          setOptions(await fetchProducts.data.products);
         else {
           setOptions([
             {
@@ -39,72 +42,55 @@ export default function Search() {
           ]);
         }
       }, 1000);
+    } catch (error) {
+      console.log(error);
     }
-    getProducts();
-  }, [open, search]);
+  }
+
   return (
-    <div className='d-flex  '>
-      <Autocomplete
-        id='asynchronous-demo'
-        sx={{ width: 300, border: 'unset', borderRadius: 'unset' }}
-        open={open}
-        onOpen={() => {
-          setOpen(true);
+    <div className='d-flex position-relative '>
+      <input
+        type='text'
+        className='form-control'
+        placeholder='Search'
+        value={search}
+        onBlur={() =>
+          setTimeout(() => {
+            setOpen(false);
+          }, 200)
+        }
+        onFocus={() => setOpen(true)}
+        onChange={(e) => {
+          setSearch(e.target.value || '');
         }}
-        onClose={() => {
-          setOpen(false);
-        }}
-        isOptionEqualToValue={(option, value) => option.slug === value.slug}
-        getOptionLabel={(option) => option.name}
-        options={options}
-        loading={loading}
-        renderInput={(params) => (
-          <TextField
-            value={search}
-            onChange={async (e) => {
-              setSearch(e.target.value || '');
-              // let fetchProducts = await Request.get(
-              //   Api.SEARCH_PRODUCTS + e.target.value
-              // );
-              // setProducts(await fetchProducts.data);
-            }}
-            {...params}
-            label='Search'
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {loading ? (
-                    <CircularProgress color='inherit' size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              ),
-            }}
-          />
-        )}
       />
-      <div className='input-group-append'>
-        <span className='input-group-text bg-primary border-primary text-white'>
-          <i className='fa fa-search' />
+
+      <div
+        onClick={() => router.push(`/shop?name=${search}`)}
+        className='input-group-append'>
+        <span className='input-group-text border-primary text-white'>
+          <i className='fa fa-search text-primary' />
         </span>
       </div>
+      {open && (
+        <div
+          className='w-100 search-menu position-absolute rounded shadow bg-white '
+          style={{ zIndex: 1, top: '50px' }}>
+          {products.map((product: Product, index: number) => (
+            <div
+              key={index}
+              className='w-100 h-100 d-flex flex-column justify-content-center'>
+              <Link
+                href={`/details?slug=${product.slug}`}
+                key={product.slug + index}
+                onClick={() => setOpen(false)}
+                className='text-decoration-none mx-3 my-1 nav-link text-dark'>
+                {product.name}
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
-// const products = async (search: string = '') => {
-//   try {
-//     const data = await Request.get(Api.SEARCH_PRODUCTS);
-//     if (data.success) {
-//       return data.data.map((product: Product) => ({
-//         name: product.name,
-//         slug: product.slug,
-//       }));
-//     } else {
-//       return [];
-//     }
-//   } catch {
-//     return [];
-//   }
-// };
