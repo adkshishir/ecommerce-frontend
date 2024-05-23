e64a2a60614987124ee9e165999851fe4caaf505
adkshishir <adhikarishishir50@gmail.com>
2024-05-23 10:41:30 +0545

    update : end points

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,6 +1,6 @@
 const BASE_URL = process.env.BASE_URL || process.env.NEXT_PUBLIC_BASE_URL;
 const HOME_PAGE = `${BASE_URL}/api/home-page-data`;
-const SEARCH_PRODUCTS = `${BASE_URL}/api/search-products?search=`;
+const SEARCH_PRODUCTS = `${BASE_URL}/api/search-products`;
 const LOGIN = `${BASE_URL}/api/auth/login`;
 const SIGNUP = `${BASE_URL}/api/auht/register`;
 const PARENT_CATEGORIES = `${BASE_URL}/api/parent-categories`;
