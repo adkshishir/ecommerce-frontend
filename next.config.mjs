c97979d5f5655c75d441b61d59d592dbbaeed169
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 07:50:05 +0545

    add : Image hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "fakestoreapi.com",
-            },
-            {
-                protocol: "http",
-                hostname: "localhost",
-                port: "8000",
-                pathname: "/**",
-            }, {
-                protocol:"https",
-                hostname: "picsum.photos",
-                
-            }
-        ],
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        hostname: 'loremflickr.com',
+        protocol: 'https',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
