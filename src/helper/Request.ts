d11dd540f10e990da6985286361a51111edf6ba2
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 08:04:15 +0545

    add : delete request

--- src/helper/Request.ts
+++ src/helper/Request.ts
@@ -1,6 +1,11 @@
-async function get(url: string) {
+async function get(url: string, token?: string) {
   try {
-    const response = await fetch(url,{cache:'no-cache'});
+    const response = await fetch(url, {
+      cache: 'no-cache',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     const data = await response.json();
     return data;
   } catch (error: any) {
@@ -11,15 +16,35 @@ async function get(url: string) {
     };
   }
 }
-async function post(url: string, formData: any) {
+async function post(url: string, formData: any, token?: string) {
   try {
     const response: Response = await fetch(url, {
       method: 'POST',
       cache: 'no-cache',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(formData),
+    });
+    const responseData = await response.json();
+    console.log(responseData);
+    return responseData;
+  } catch (error) {
+    return {
+      success: false,
+      data: [],
+    };
+  }
+}
+async function deleteReq(url: string, token?: string) {
+  try {
+    const response: Response = await fetch(url, {
+      method: 'DELETE',
+      cache: 'no-cache',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
     const responseData = await response.json();
     console.log(responseData);
@@ -34,4 +59,5 @@ async function post(url: string, formData: any) {
 export default {
   get,
   post,
+  delete: deleteReq,
 };
