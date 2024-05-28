df588b49ba578cb0c64451c0a0642873ff70112f
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 08:02:48 +0545

    add : profile on load

--- src/components/SetData.tsx
+++ src/components/SetData.tsx
@@ -1,26 +1,25 @@
 'use client';
 import React, { useContext, useEffect } from 'react';
-import { homeDataResponseType, productType } from '../types/types';
+import { homeDataResponseType, productType, profileType } from '../types/types';
 import { MyContext } from '../context/MyProvider';
 import Request from '../helper/Request';
 import Api from '../constants/Api';
 
-const SetData = ({
-  homeData,
-  products,
-}: {
-  homeData: homeDataResponseType;
-  products: productType[];
-}) => {
-  const { setHomePageData, setProducts } = useContext(MyContext);
+const SetData = () => {
+  const { setHomePageData, setProducts, setProfile } = useContext(MyContext);
   useEffect(() => {
     const fetchData = async () => {
       const homeData: homeDataResponseType = await Request.get(Api.HOME_PAGE);
       const products = await Request.get(Api.SEARCH_PRODUCTS);
+      const profile = await Request.get(
+        Api.AUTH_PROFILE,
+        localStorage?.getItem('token') || ''
+      );
       setHomePageData({
         ...homeData,
       });
       setProducts(products?.data?.products);
+      setProfile(profile?.data);
     };
 
     fetchData();
