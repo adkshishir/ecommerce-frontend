1da48220cfea8d89be48e3500de1d5399acf6352
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 08:12:36 +0545

    remove : unused chuncks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Request from '../helper/Request';
-import Api from '../constants/Api';
-import { homeDataResponseType } from '../types/types';
 import { Toaster } from 'react-hot-toast';
 const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
