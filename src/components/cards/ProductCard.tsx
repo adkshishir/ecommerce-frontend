7af77a46b7c62820d27c01f26ec8e3cbfc7d368b
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 08:03:01 +0545

    add : profile

--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -1,8 +1,13 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import Request from '@/src/helper/Request';
+import Api from '@/src/constants/Api';
+import toast from 'react-hot-toast';
+import Button from '../Button';
+import { MyContext } from '@/src/context/MyProvider';
 type ProductCardProps = {
   image: string;
   name: string;
@@ -12,6 +17,7 @@ type ProductCardProps = {
   className?: string;
   alt: string;
   slug: string;
+  id: number;
 };
 const ProductCard = ({
   image,
@@ -22,10 +28,32 @@ const ProductCard = ({
   alt,
   discount,
   slug,
+  id,
 }: ProductCardProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  function handleCartStore() {}
+  const { setProfile } = useContext(MyContext);
+  async function handleCartStore() {
+    try {
+      await Request.post(
+        Api.CART,
+        {
+          productId: id,
+          quantity: 1,
+        },
+        localStorage.getItem('token') || ''
+      );
+      let profile = await Request.get(
+        Api.AUTH_PROFILE,
+        localStorage.getItem('token') || ''
+      );
+      setProfile(profile?.data);
+      toast.success('Product added successfully');
+      // getCart();
+    } catch (error) {
+      toast.error('Error while adding product');
+    }
+  }
   function handleWishList() {}
   function handleRefresh() {}
   function handleSearch() {
@@ -46,40 +74,34 @@ const ProductCard = ({
             alt={alt}
           />
           <div className='product-action'>
-            <Link
-              href={'#'}
-              className='btn btn-outline-dark btn-square'
-              onClick={handleCartStore}>
+            <Button
+              onClick={handleCartStore}
+              className='btn btn-outline-dark btn-square'>
               <i className='fa fa-shopping-cart' />
-            </Link>
-            <Link
-              href={'#'}
-              className='btn btn-outline-dark btn-square'
-              onClick={handleWishList}>
-              <i className='far fa-heart' />
-            </Link>
-            {/* <Link
-              href={'#'}
-              className='btn btn-outline-dark btn-square'
-              onClick={handleRefresh}>
-              <i className='fa fa-sync-alt' />
-            </Link> */}
-            <a
-              href={'#'}
-              className='btn btn-outline-dark btn-square'
-              onClick={handleSearch}>
+            </Button>
+            <Button
+              onClick={handleWishList}
+              className='btn btn-outline-dark btn-square'>
+              <i className='fa fa-heart' />
+            </Button>
+
+            <Button
+              onClick={handleSearch}
+              className='btn btn-outline-dark btn-square'>
               <i className='fa fa-search' />
-            </a>
+            </Button>
           </div>
         </div>
         <div className='text-center py-4'>
-          <a className='h6 text-decoration-none text-truncate' href={`details?slug=${slug}`}>
+          <a
+            className='h6 text-decoration-none text-truncate'
+            href={`details?slug=${slug}`}>
             {name}
           </a>
           <div className='d-flex align-items-center justify-content-center mt-2'>
             <h5>{markedPrice - discount}</h5>
             <h6 className='text-muted ml-2'>
-              <del>${markedPrice}</del>
+              <del>Rs{markedPrice}</del>
             </h6>
           </div>
           <div className='d-flex align-items-center justify-content-center mb-1'>
